1e04228a76ad6d3c168d27223a6440bd5c70934a
Joshua Owens <jko113@gmail.com>
2018-07-25 15:36:44 -0400

    adding scroll through gallery feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,20 @@ class App extends Component {
     super(props);
 
     this.state = {
-      imgUrls:["https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg","https://proxy.topixcdn.com/ipicimg/R0GN8TA70MQ8FF5N-cp0x237x1600x1038-fill810x415x","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSWeFeN5gO_iCKZSZv22FsI_GnSQNLqYZLZ9t5BP91X8Pm150M","https://static1.comicvine.com/uploads/original/6/63459/5391391-8476188794-_8682.png"],
+      imgUrls:[
+        {id: 0,
+        url: "https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
+        score: 0},
+        {id: 1,
+        url: "https://proxy.topixcdn.com/ipicimg/R0GN8TA70MQ8FF5N-cp0x237x1600x1038-fill810x415x",
+        score: 0},
+        {id: 2,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSWeFeN5gO_iCKZSZv22FsI_GnSQNLqYZLZ9t5BP91X8Pm150M",
+        score: 0},
+        {id: 3,
+        url: "https://static1.comicvine.com/uploads/original/6/63459/5391391-8476188794-_8682.png",
+        score: 0},
+      ],
       currentImage: "https://proxy.topixcdn.com/ipicimg/R0GN8TA70MQ8FF5N-cp0x237x1600x1038-fill810x415x",
     };
   };
@@ -17,8 +30,6 @@ class App extends Component {
       <div className="App">
         <div className="gallery">
           <button className="button" onClick={this._switchImageBackward}>&lt;</button>
-          {/* <Image imgUrl="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg" /> */}
-          {/* {this._convertImgUrlsToImgElements()} */}
           {this._convertImgUrlToImgElement(this._getCurrentImage())}
           <button className="button" onClick={this._switchImageForward}>&gt;</button>
         </div>
@@ -44,8 +55,9 @@ class App extends Component {
   _switchImageForward = () => {
     let newCurrentImage;
     let newCurrentImageIndex = 0;
+    const imgUrls = this._getImgUrls();
     
-    this.state.imgUrls.forEach((url, index) => {
+    imgUrls.forEach((url, index) => {
       if (url === this.state.currentImage) {
         newCurrentImageIndex = index + 1;
       }
@@ -55,7 +67,7 @@ class App extends Component {
       newCurrentImageIndex = 0;
     }
     
-    newCurrentImage = this.state.imgUrls[newCurrentImageIndex];
+    newCurrentImage = imgUrls[newCurrentImageIndex];
 
     this.setState({
       currentImage: newCurrentImage,
@@ -64,8 +76,9 @@ class App extends Component {
   _switchImageBackward = () => {
     let newCurrentImage;
     let newCurrentImageIndex = 0;
+    const imgUrls = this._getImgUrls();
     
-    this.state.imgUrls.forEach((url, index) => {
+    imgUrls.forEach((url, index) => {
       if (url === this.state.currentImage) {
         newCurrentImageIndex = index - 1;
       }
@@ -75,7 +88,7 @@ class App extends Component {
       newCurrentImageIndex = this.state.imgUrls.length - 1;
     }
     
-    newCurrentImage = this.state.imgUrls[newCurrentImageIndex];
+    newCurrentImage = imgUrls[newCurrentImageIndex];
 
     this.setState({
       currentImage: newCurrentImage,
@@ -89,10 +102,19 @@ class App extends Component {
   }
 
   _getCurrentImage = () => {
-    const returnThing = this.state.imgUrls.find(url => {
+    const imgUrls = this._getImgUrls();
+    const returnThing = imgUrls.find(url => {
       return url === this.state.currentImage;
     });
     return returnThing;
+  }
+
+  _getImgUrls = () => {
+    let arrayOfUrls = [];
+    this.state.imgUrls.forEach(obj => {
+      arrayOfUrls.push(obj.url);
+    });
+    return arrayOfUrls;
   }
 }
 
